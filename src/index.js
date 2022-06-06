af53f81dbd75708cff26d772089cf4aa5222efb8
Bohdan Sichkaruk <82411127+Sichkaruk@users.noreply.github.com>
2022-06-06 23:55:12 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./components/constants/theme";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
